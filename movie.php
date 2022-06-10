4af55146933505e350371acd3df6e3cf9f23423b
Maxnightik <98279735+Maxnightik@users.noreply.github.com>
2022-06-10 21:52:59 +0300

    addmovie

---
+++ movie.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>KinoBanda</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header>
+        <div class="header-top">
+            <div class="container">
+                <div class="logo"><img src="image/logo.png" alt="logo" /></div>
+                <div class="search"><img src="image/search.svg" alt="search" /></div>
+            </div>
+        </div>
+    </header>
+
+    <!-- END HEADER -->
+
+    <!-- MAIN -->
+    <main>
+        <div class="container">
+            <div class="movies">
+                <div class="movie">
+                    <h2 class="movie-title">
+                        Обі-Ван Кенобі / Obi-Wan Kenobi (2022)
+                    </h2>
+                    <img src="image/poster.jpg" alt="постер" />
+                    <div class="description">
+                        <div class="genre">Фантастика, Бойовик, пригоди</div>
+                        <p>2022</p>
+                        <p>
+                            Спін-офф із франшизи «Зоряних війн», присвяченої майстру-джедаю
+                            Обі-Вану Кенобі. Події розгорнуться через десять років після
+                            того, що сталося в «Помсті сітхів», коли Обі-Ван Кенобі доставив
+                            немовля Люка Скайуокера на Татуїн.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="movie">
+                    <h2 class="movie-title">Обі-Ван Кенобі / Obi-Wan Kenobi (2022)</h2>
+                    <img src="image/poster.jpg" alt="постер" />
+                    <div class="description">
+                        <div class="genre">Фантастика, Бойовик, пригоди</div>
+                        <p>
+                            Спін-офф із франшизи «Зоряних війн», присвяченої майстру-джедаю
+                            Обі-Вану Кенобі. Події розгорнуться через десять років після
+                            того, що сталося в «Помсті сітхів», коли Обі-Ван Кенобі доставив
+                            немовля Люка Скайуокера на Татуїн.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="movie">
+                    <h2 class="movie-title">Обі-Ван Кенобі / Obi-Wan Kenobi (2022)</h2>
+                    <img src="image/poster.jpg" alt="постер" />
+                    <div class="description">
+                        <div class="genre">Фантастика, Бойовик, пригоди</div>
+                        <p>
+                            Спін-офф із франшизи «Зоряних війн», присвяченої майстру-джедаю
+                            Обі-Вану Кенобі. Події розгорнуться через десять років після
+                            того, що сталося в «Помсті сітхів», коли Обі-Ван Кенобі доставив
+                            немовля Люка Скайуокера на Татуїн.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="movie">
+                    <h2 class="movie-title">
+                        Обі-Ван Кенобі / Obi-Wan Kenobi (2022)
+                    </h2>
+                    <img src="image/poster.jpg" alt="постер" />
+                    <div class="description">
+                        <div class="genre">Фантастика, Бойовик, пригоди</div>
+                        <p>
+                            Спін-офф із франшизи «Зоряних війн», присвяченої майстру-джедаю
+                            Обі-Вану Кенобі. Події розгорнуться через десять років після
+                            того, що сталося в «Помсті сітхів», коли Обі-Ван Кенобі доставив
+                            немовля Люка Скайуокера на Татуїн.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- END MAIN -->
+
+    <!-- FOOTER -->
+    <footer>
+        <div class="footer-container">
+            <div class="logo">
+                <img src="image/logo.png" alt="logo" />
+            </div>
+        </div>
+    </footer>
+
+    <!-- END FOOTER -->
+</body>
+
+</html>
