f6ec8a497777fb39ffe1721e1e8e4676f5a33122
halyna20 <halynka2000@gmail.com>
2022-06-15 22:46:26 +0300

    fix

--- pages/movie.php
+++ pages/movie.php
@@ -2,64 +2,64 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . '/config/db.php';
 
 if (isset($_POST) and $_SERVER["REQUEST_METHOD"] == "POST") {
-	$str = '';
+    $str = '';
 
-	if(isset($_POST['search__text']) && $_POST['search__text'] != "") {
-		$str .= "movieName LIKE '%" . $_POST['search__text'] . "%'";
-	}
+    if (isset($_POST['search__text']) && $_POST['search__text'] != "") {
+        $str .= "movieName LIKE '%" . $_POST['search__text'] . "%'";
+    }
 
-	if(isset($_POST['filter'])) {
-    $filter = $_POST['filter'];
-    for ($i = 0; $i < count($filter); $i++) {
-        $strLenght = strlen($str);
-        if ($filter[$i]['name'] == 'type' && $filter[$i]['value'] != '') {
-            if ($strLenght > 0) {
-                $str .= " AND ";
-            }
-            $str .= "characterId = " . $filter[$i]['value'];
-        }
-
-        if ($filter[$i]['name'] == 'year-from' && $filter[$i]['value'] != '') {
-            if (is_numeric($filter[$i]['value'])) {
+    if (isset($_POST['filter'])) {
+        $filter = $_POST['filter'];
+        for ($i = 0; $i < count($filter); $i++) {
+            $strLenght = strlen($str);
+            if ($filter[$i]['name'] == 'type' && $filter[$i]['value'] != '') {
                 if ($strLenght > 0) {
                     $str .= " AND ";
                 }
-                $str .= "(year >= " . $filter[$i]['value'] . ")";
+                $str .= "characterId = " . $filter[$i]['value'];
             }
-        }
 
-        if ($filter[$i]['name'] == 'year-to' && $filter[$i]['value'] != '') {
-            if (is_numeric($filter[$i]['value'])) {
-                if ($strLenght > 0) {
-                    $str .= " AND ";
-                }
-                $str .= "(year <= " . $filter[$i]['value'] . ")";
-            }
-        }
-
-        if ($filter[$i]['categories']) {
-
-            for ($j = 0; $j < count($filter[$i]['categories']); $j++) {
-                if (count($filter[$i]['categories']) > 1) {
-                    if ($j == 0) {
-                        $str .= " AND (categoryId = " . $filter[$i]['categories'][$j];
-                    } else {
-                        $str .= " OR categoryId = " . $filter[$i]['categories'][$j];
-                    }
-                    if ($j == count($filter[$i]['categories']) - 1) {
-                        $str .= ")";
-                    }
-                } else {
+            if ($filter[$i]['name'] == 'year-from' && $filter[$i]['value'] != '') {
+                if (is_numeric($filter[$i]['value'])) {
                     if ($strLenght > 0) {
                         $str .= " AND ";
                     }
+                    $str .= "(year >= " . $filter[$i]['value'] . ")";
+                }
+            }
 
-                    $str .= " categoryId = " . $filter[$i]['categories'][$j];
+            if ($filter[$i]['name'] == 'year-to' && $filter[$i]['value'] != '') {
+                if (is_numeric($filter[$i]['value'])) {
+                    if ($strLenght > 0) {
+                        $str .= " AND ";
+                    }
+                    $str .= "(year <= " . $filter[$i]['value'] . ")";
+                }
+            }
+
+            if ($filter[$i]['categories']) {
+
+                for ($j = 0; $j < count($filter[$i]['categories']); $j++) {
+                    if (count($filter[$i]['categories']) > 1) {
+                        if ($j == 0) {
+                            $str .= " AND (categoryId = " . $filter[$i]['categories'][$j];
+                        } else {
+                            $str .= " OR categoryId = " . $filter[$i]['categories'][$j];
+                        }
+                        if ($j == count($filter[$i]['categories']) - 1) {
+                            $str .= ")";
+                        }
+                    } else {
+                        if ($strLenght > 0) {
+                            $str .= " AND ";
+                        }
+
+                        $str .= " categoryId = " . $filter[$i]['categories'][$j];
+                    }
                 }
             }
         }
     }
-	}
 }
 ?>
 
